d7cd54717612dcd992073126cabbf1c50cf7f0d2
Aren <aerenevli@gmail.com>
2026-07-05 12:20:58 +0300

    Scaffold About section and wire it into nav and page
    Adds an empty AboutSection (content/design to follow) placed between
    Home and Courses, and adds a "Hakkımızda" link to navLinks so it shows
    up in both the desktop nav and mobile menu, which share that array.

---
+++ components/sections/AboutSection.tsx
@@ -0,0 +1,9 @@
+export function AboutSection() {
+  return (
+    <section id="about" className="relative scroll-mt-16 bg-background py-32">
+      <div className="mx-auto max-w-7xl px-5 md:px-16">
+        {/* TODO: content and design to be added */}
+      </div>
+    </section>
+  );
+}
